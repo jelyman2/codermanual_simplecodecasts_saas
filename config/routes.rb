3a6cdb4e4a94ab149431146c5d58007414150804
Johnathan Lyman <jelyman@gmail.com>
2015-10-25 16:46:53 +0000

    Added Stripe js

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,11 @@ Rails.application.routes.draw do
   root 'pages#home'
   get '/about' => 'pages#about'
   resources :contacts
-    
-    
-    
-    
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
