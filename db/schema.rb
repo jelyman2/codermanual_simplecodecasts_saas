7c76ca2342dba17aaad36e0e8c856ee1fd955e6f
Johnathan Lyman <jelyman@gmail.com>
2015-10-25 17:04:42 +0000

    Modified Devise for stripe

--- db/schema.rb
+++ db/schema.rb
@@ -11,37 +11,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151025000458) do
+ActiveRecord::Schema.define(version: 20151025170038) do
 
-  create_table "contacts", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
+  create_table "contacts", force: true do |t|
+    t.string   "name",       limit: nil
+    t.string   "email",      limit: nil
     t.text     "comments"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "plans", force: :cascade do |t|
-    t.string   "name"
+  create_table "plans", force: true do |t|
+    t.string   "name",       limit: nil
     t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "users", force: true do |t|
+    t.string   "email",                  limit: nil, default: "", null: false
+    t.string   "encrypted_password",     limit: nil, default: "", null: false
+    t.string   "reset_password_token",   limit: nil
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip",     limit: nil
+    t.string   "last_sign_in_ip",        limit: nil
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "plan_id"
+    t.string   "stripe_customer_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
