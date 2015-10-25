6d9766375adfafb08c98cfed8899801a00815d42
Johnathan Lyman <jelyman@gmail.com>
2015-10-25 23:32:55 +0000

    Updated profile form and added create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,5 +3,26 @@ class ProfilesController < ApplicationController
     # form where a user can fill out their own profile.
     @user = User.find(params[:user_id])
     @profile = @user.build_profile
+    @default_user_email = @user.email
   end
+  
+  def create
+    @user = User.find(params[:user_id])
+    @profile = @user.build_profile(profile_params)
+    if @profile.save
+      flash[:success] = "Your profile was updated successfully!"
+      redirect_to user_path(params[:user_id])
+    else
+      flash[:danger] = "There was a problem updating your profile. :("
+      render action: :new
+    end
+  end
+  
+  private
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :location_city, 
+      :location_state, :job_title, :job_company, :phone_number, :contact_email, 
+      :website, :social_github, :social_twitter, :social_facebook, 
+      :social_linkedin, :description)
+    end
 end
